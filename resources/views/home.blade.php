086f915cbf85f21a1caf22f7e1647234776afaae
Oricy <d.duvernois@student.isartdigital.com>
2016-05-20 14:04:36 +0200

    Add no quiz situation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,21 @@
 
 @section('content')
 <div class="panel-heading">
-    <h2 class="text-center">{{ $quiz->title }}</h2>
+    @if($quiz)
+        <h2 class="text-center">{{ $quiz->title }}</h2>
+    @else
+        <h2 class="text-center">Quiz</h2>
+    @endif
 </div>
 
 <div class="panel-body">
     <div class="col-md-2 col-md-offset-5">
+        @if($quiz)
+            <a class="btn btn-block btn-primary" href="{{ route('home.game', [$quiz->id, 0]) }}" role="button">Link</a>
+        @else
+            <p class="text-center">Il n'y a aucun quiz pour l'instant</p>
+        @endif
         <!--<a class="btn btn-block btn-primary" href="{{-- route('home.game', [$quiz->id, $question[0]->id]) --}}" role="button">Link</a>-->
-        <a class="btn btn-block btn-primary" href="{{ route('home.game', [$quiz->id, 0]) }}" role="button">Link</a>
     </div>
 </div>
 @endsection
